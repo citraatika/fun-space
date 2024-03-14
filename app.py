37a5e98500272ee67f0693845e5372b7022a3a1a
Citra atika <162818076+citraatika@users.noreply.github.com>
2024-03-14 22:01:57 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-# Hitung Luas Segitiga sederhana
+# Hitung Luas Segitiga
+
 def luas_segitiga():
-    a = int(input("Masukkan alas segitiga:"))
+    a = int(input("Masukkan alas segitiga: "))
     t = int(input("Masukkan tinggi segitiga: "))
     luas = a * t / 2
     print("Luas segitiga adalah: ", luas)
 
 luas_segitiga()
+
+# Hitung Luas Persegi Panjang
+def luas_persegi_panjang():
+    p = int(input("Masukkan panjang persegi: "))
+    l = int(input("Masukkan lebar persegi: "))
+    luas = p * 1
+    print("luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
